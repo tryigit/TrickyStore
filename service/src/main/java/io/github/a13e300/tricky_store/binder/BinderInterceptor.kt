6e02f1e476d7c8fd011d82e5a77cf14078040cf1
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-11 10:35:59 +0300

    Update BinderInterceptor.kt

--- service/src/main/java/io/github/a13e300/tricky_store/binder/BinderInterceptor.kt
+++ service/src/main/java/io/github/a13e300/tricky_store/binder/BinderInterceptor.kt
@@ -53,9 +53,14 @@ open class BinderInterceptor : Binder() {
                 val callingUid = data.readInt()
                 val callingPid = data.readInt()
                 val sz = data.readLong()
+
+                // Use a temporary buffer to avoid creating a new Parcel
+                val buffer = ByteArray(sz.toInt())
+                data.readByteArray(buffer)
+
                 val theData = Parcel.obtain()
                 try {
-                    theData.appendFrom(data, data.dataPosition(), sz.toInt())
+                    theData.unmarshall(buffer, 0, buffer.size)
                     theData.setDataPosition(0)
                     onPreTransact(target, theCode, theFlags, callingUid, callingPid, theData)
                 } finally {
