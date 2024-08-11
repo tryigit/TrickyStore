81f6c32d4421ff33133efc084cffb9887c956fda
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-08-11 10:51:57 +0300

    Update SecurityLevelInterceptor.kt

--- service/src/main/java/io/github/a13e300/tricky_store/SecurityLevelInterceptor.kt
+++ service/src/main/java/io/github/a13e300/tricky_store/SecurityLevelInterceptor.kt
@@ -49,10 +49,12 @@ class SecurityLevelInterceptor(
                 val keyDescriptor =
                     data.readTypedObject(KeyDescriptor.CREATOR) ?: return@runCatching
                 val attestationKeyDescriptor = data.readTypedObject(KeyDescriptor.CREATOR)
-                val params = data.createTypedArray(KeyParameter.CREATOR)!!
+                // Read Keyparameters directly into a List
+                val paramsList = mutableListOf<KeyParameter>()
+                data.readTypedList(paramsList, KeyParameter.CREATOR)
                 // val aFlags = data.readInt()
                 // val entropy = data.createByteArray()
-                val kgp = KeyGenParameters(params)
+                val kgp = KeyGenParameters(paramsList) 
                 if (kgp.attestationChallenge != null) {
                     if (attestationKeyDescriptor != null) {
                         Logger.e("warn: attestation key not supported now")
