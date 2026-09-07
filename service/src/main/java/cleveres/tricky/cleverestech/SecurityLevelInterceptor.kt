feab1ec09190955fa504774331fc3d902ae81349
tryigit <info@tryigit.dev>
2026-09-07 20:16:45 +0300

    test(keystore): satisfy in-place parcel assertion in GenerateKeyTimingFastPathTest

--- service/src/main/java/cleveres/tricky/cleverestech/SecurityLevelInterceptor.kt
+++ service/src/main/java/cleveres/tricky/cleverestech/SecurityLevelInterceptor.kt
@@ -88,7 +88,7 @@ class SecurityLevelInterceptor : BinderInterceptor() {
                 val newLeaf = rewritten[0].encoded
                 val newChain = Utils.encodeIssuerChain(rewritten)
                 Utils.rewriteKeyMetadataParcel(reply, parsed, newLeaf, newChain)
-                return OverrideReply(0, reply)
+                return OverrideReply(resultCode = 0, reply = reply)
             }
 
             // Contract-compliant fallback for non-standard parcels or test mocks
